423715dbf1c7923846451c41bc553df51a1d6773
Hannah McLaughlin <h@mcla.ug>
2021-01-10 15:45:22 +0000

    implement register file

--- register_file.py
+++ register_file.py
@@ -16,11 +16,39 @@ class RegisterFile(nm.Elaboratable):
     """
 
     def __init__(self, num_registers=32, register_width=32):
-        self.read_select_1 = nm.Signal(range(num_registers))
-        self.read_select_2 = nm.Signal(range(num_registers))
-        self.read_data_1 = nm.Signal(register_width)
-        self.read_data_2 = nm.Signal(register_width)
+        self.num_registers = num_registers
+        self.register_width = register_width
+
+        self.read_select_1 = nm.Signal(range(self.num_registers))
+        self.read_select_2 = nm.Signal(range(self.num_registers))
+        self.read_data_1 = nm.Signal(self.register_width)
+        self.read_data_2 = nm.Signal(self.register_width)
 
         self.write_enable = nm.Signal()
-        self.write_select = nm.Signal(register_width)
-        self.write_data = nm.Signal(register_width)
+        self.write_select = nm.Signal(self.register_width)
+        self.write_data = nm.Signal(self.register_width)
+
+    def elaborate(self, _):
+        m = nm.Module()
+        registers = nm.Memory(
+                width=self.register_width,
+                depth=self.num_registers)
+
+        # In iCE40 block RAMs are always read-before-write
+        rp1 = m.submodules.rp1 = registers.read_port(transparent=False)
+        rp2 = m.submodules.rp2 = registers.read_port(transparent=False)
+        wp = m.submodules.wp = registers.write_port()
+
+        # The first register, x0, has a special function: Reading it always
+        # returns 0 and writes to it are ignored.
+        # https://github.com/riscv/riscv-asm-manual/blob/master/riscv-asm.md
+        m.d.comb += wp.en.eq(
+                nm.Mux(self.write_select == 0, 0, self.write_enable))
+
+        for sel, data, port in zip(
+                (self.read_select_1, self.read_data_1, rp1),
+                (self.read_select_2, self.read_data_2, rp2),
+                (self.write_select, self.write_data, wp)):
+            m.d.comb += [port.addr.eq(sel), port.data.eq(data)]
+
+        return m
