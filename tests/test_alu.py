6821efea81b31c6fdc7014eccfc246bef2755bd8
Hannah McLaughlin <h@mcla.ug>
2021-01-24 18:58:18 +0000

    simplify test for cominatorial logic

--- tests/test_alu.py
+++ tests/test_alu.py
@@ -1,5 +1,4 @@
 """ALU tests"""
-import nmigen as nm
 import nmigen.sim
 
 from riscy_boi import alu
@@ -7,21 +6,15 @@ from riscy_boi import alu
 
 def test_alu():
     alu_inst = alu.ALU(32)
-    mod = nm.Module()
-    mod.submodules.alu = alu_inst
-    o = nm.Signal.like(alu_inst.o)
-    mod.d.sync += o.eq(alu_inst.o)
 
     def testbench():
         yield alu_inst.op.eq(alu.ALUOp.ADD)
         yield alu_inst.a.eq(1)
         yield alu_inst.b.eq(1)
-        yield
-        yield
-        assert (yield o) == 2
+        yield nmigen.sim.Settle()
+        assert (yield alu_inst.o) == 2
 
-    sim = nmigen.sim.Simulator(mod)
-    sim.add_sync_process(testbench)
-    sim.add_clock(1e-6)
+    sim = nmigen.sim.Simulator(alu_inst)
+    sim.add_process(testbench)
     with sim.write_vcd("tests/vcd/alu.vcd"):
-        sim.run()
+        sim.run_until(100e-6)
