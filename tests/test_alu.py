335c0bfed18b451d35ba89d5852f02ce3adee19e
Hannah McLaughlin <h@mcla.ug>
2021-02-07 15:46:30 +0000

    parametrise alu test

--- tests/test_alu.py
+++ tests/test_alu.py
@@ -1,18 +1,31 @@
 """ALU tests"""
 import nmigen.sim
+import pytest
 
 from riscy_boi import alu
 
 
-def test_alu():
+@pytest.mark.parametrize(
+        "op, a, b, o", [
+            (alu.ALUOp.ADD, 1, 1, 2),
+            (alu.ALUOp.ADD, 1, 2, 3),
+            (alu.ALUOp.ADD, 2, 1, 3),
+            (alu.ALUOp.ADD, 258, 203, 461),
+            (alu.ALUOp.ADD, 5, 0, 5),
+            (alu.ALUOp.ADD, 0, 5, 5),
+            (alu.ALUOp.ADD, 2**32 - 1, 1, 0),
+            (alu.ALUOp.SUB, 1, 1, 0),
+            (alu.ALUOp.SUB, 4942, 0, 4942),
+            (alu.ALUOp.SUB, 1, 2, 2**32 - 1)])
+def test_alu(op, a, b, o):
     alu_inst = alu.ALU(32)
 
     def testbench():
-        yield alu_inst.op.eq(alu.ALUOp.ADD)
-        yield alu_inst.a.eq(1)
-        yield alu_inst.b.eq(1)
+        yield alu_inst.op.eq(op)
+        yield alu_inst.a.eq(a)
+        yield alu_inst.b.eq(b)
         yield nmigen.sim.Settle()
-        assert (yield alu_inst.o) == 2
+        assert (yield alu_inst.o) == o
 
     sim = nmigen.sim.Simulator(alu_inst)
     sim.add_process(testbench)
