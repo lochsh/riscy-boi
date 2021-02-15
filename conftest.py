72309284b773c7e5a6510a52f5d72b2bfde25091
Hannah McLaughlin <h@mcla.ug>
2021-02-15 17:35:29 +0000

    cleanup vcd files before running test session

--- conftest.py
+++ conftest.py
@@ -1,17 +1,26 @@
 """Test configuration"""
 import os
+import shutil
 
 import nmigen.sim
 import pytest
 
 
+VCD_TOP_DIR = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "tests",
+        "vcd")
+
+
 def vcd_path(node):
-    directory = os.path.join(
-            "tests",
-            "vcd",
-            node.fspath.basename.split(".")[0])
+    directory = os.path.join(VCD_TOP_DIR, node.fspath.basename.split(".")[0])
     os.makedirs(directory, exist_ok=True)
     return os.path.join(directory, node.name + ".vcd")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def clear_vcd_directory():
+    shutil.rmtree(VCD_TOP_DIR, ignore_errors=True)
 
 
 @pytest.fixture
